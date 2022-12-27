4cb8cee563955d2931dd033a3003a61b4174744c
Swelldg <mike_1018@126.com>
2022-12-27 17:16:09 +0800

    export component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ConciseScrollBar from "./ConciseScrollbar/ConciseScrollbar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
+export {ConciseScrollBar};
